88f4d466bba142994e2827f2052813bbb51f0108
Illia Suslov <97124089+iSUS38@users.noreply.github.com>
2023-03-23 19:47:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use(function(req, res, next) {
 });
 
 var connection = mysql.createConnection({
-    host: "sql7.freemysqlhosting.net",
-    user: "sql7608016",
-    password: "WhjWXgpnPw",
-    database: "sql7608016"
+    host: "db4free.net", //"sql7.freemysqlhosting.net",
+    user: "app12345",
+    password: "ffc4e31d",
+    database: "ap12345"
 });
 
 connection.connect(function(err) {
